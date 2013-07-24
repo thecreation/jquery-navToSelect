1391a2ec6f4c658046e0d723d31922a150cde2eb
amazingsurge <amazingsurge@yahoo.com>
2013-07-24 19:12:19 +0800

    udpate package.json

--- src/jquery-navtoselect.js
+++ src/jquery-navtoselect.js
@@ -1,10 +1,10 @@
-(function (document, $, undefined) {
+(function(document, $, undefined) {
   "use strict";
 
   var namespace = 'navToSelect';
 
   // Constructor
-  var Plugin = $.navToSelect = function (nav, settings) {
+  var Plugin = $.navToSelect = function(nav, settings) {
     this.nav = nav;
     this.$nav = $(nav);
 
@@ -28,15 +28,15 @@
     indentSpace: true,
     defaultText: 'Navigate to...',
     useOptgroup: false,
-    changeEvent: function(){
-      if($(this).data('linkable')!==false){
+    changeEvent: function() {
+      if ($(this).data('linkable') !== false) {
         Plugin.goTo(this.value);
       }
     }
   };
 
   // Plugin initializer - prepare your plugin.
-  Plugin.prototype.init = function () {
+  Plugin.prototype.init = function() {
     var items = this.getItems();
     this.build(items);
 
@@ -44,56 +44,57 @@
 
   };
 
-  Plugin.prototype.build = function(items){
+  Plugin.prototype.build = function(items) {
     this.$select = $('<select />', {
-      'class':this.settings.className
-    }).html(this.generateOptionsString(items,1));
+      'class': this.settings.className
+    }).html(this.generateOptionsString(items, 1));
 
-    if(this.settings.prependTo === null){
+    if (this.settings.prependTo === null) {
       this.$nav.after(this.$select);
-    }else{
+    } else {
       this.$select.prependTo(this.settings.prependTo);
     }
     this._isBuilded = true;
   };
 
-  Plugin.prototype.generateOptionString = function(item, level){
-    var indent = new Array( level ).join( this.settings.indentString);
-    if(level !== 1 && this.settings.indentSpace) {
+  Plugin.prototype.generateOptionString = function(item, level) {
+    var indent = new Array(level).join(this.settings.indentString);
+    if (level !== 1 && this.settings.indentSpace) {
       indent += '&nbsp;';
     }
-    return '<option value="'+item.value+'"'+
-      (item.linkable === false?' data-linkable="false"':'')+
-      (item.actived === true?' selected="selected"':'')+
-      '>'+indent+item.label+'</option>';
+    return '<option value="' + item.value + '"' +
+      (item.linkable === false ? ' data-linkable="false"' : '') +
+      (item.actived === true ? ' selected="selected"' : '') +
+      '>' + indent + item.label + '</option>';
   };
 
-  Plugin.prototype.generateOptionsString = function(items, level){
-    if (level > this.settings.maxLevel){
+  Plugin.prototype.generateOptionsString = function(items, level) {
+    if (level > this.settings.maxLevel) {
       return '';
     }
     var options = '';
     var self = this;
-      $.each(items, function(index, item){
-        if(item.linkable === false && typeof item.items !== 'undefined' && level === 1 && self.settings.useOptgroup){
-          options += '<optgroup label="'+item.label+'">';
-          options += self.generateOptionsString(item.items,level+1);
-          options += '</optgroup>';
-        }if(typeof item.items !== 'undefined'){
-          options += self.generateOptionString(item, level);
-          options += self.generateOptionsString(item.items,level+1);
-        }else{
-          options += self.generateOptionString(item, level);
-        }
-      });
-      return options;
+    $.each(items, function(index, item) {
+      if (item.linkable === false && typeof item.items !== 'undefined' && level === 1 && self.settings.useOptgroup) {
+        options += '<optgroup label="' + item.label + '">';
+        options += self.generateOptionsString(item.items, level + 1);
+        options += '</optgroup>';
+      }
+      if (typeof item.items !== 'undefined') {
+        options += self.generateOptionString(item, level);
+        options += self.generateOptionsString(item.items, level + 1);
+      } else {
+        options += self.generateOptionString(item, level);
+      }
+    });
+    return options;
   };
 
   Plugin.prototype.getItems = function() {
     var items = [];
-    if(this.settings.defaultText){
+    if (this.settings.defaultText) {
       items = items.concat({
-        value:"#",
+        value: "#",
         label: this.settings.defaultText,
         linkable: false
       });
@@ -101,10 +102,11 @@
     var self = this;
 
     //recursion
-    function _getItemsFromList($list, level){
+
+    function _getItemsFromList($list, level) {
       var _items = [];
 
-      $list.children('li').each(function(){
+      $list.children('li').each(function() {
         var $li = $(this);
         var item = {
           value: self.getItemValue($li),
@@ -112,10 +114,10 @@
           linkable: self.isLinkable($li),
           actived: self.isActived($li)
         };
-        if($li.children('ul, ol').length){
+        if ($li.children('ul, ol').length) {
           item.items = [];
-          $li.children('ul, ol').each(function(){
-            item.items = item.items.concat( _getItemsFromList($(this), level+1));
+          $li.children('ul, ol').each(function() {
+            item.items = item.items.concat(_getItemsFromList($(this), level + 1));
           });
         }
 
@@ -124,27 +126,27 @@
       return _items;
     }
 
-    items = items.concat(_getItemsFromList(this.$nav,1));
+    items = items.concat(_getItemsFromList(this.$nav, 1));
     return items;
   };
 
   // Get link url
-  Plugin.prototype.getItemValue = function($li){
+  Plugin.prototype.getItemValue = function($li) {
     return $li.find(this.settings.linkSelector).attr('href');
   };
 
   // Check if a item can link
-  Plugin.prototype.isLinkable = function($li){
+  Plugin.prototype.isLinkable = function($li) {
     return this.getItemValue($li) !== '#';
   };
 
   // Check if a item is actived
-  Plugin.prototype.isActived = function($li){
-    return $li.is('.'+this.settings.activeClass);
+  Plugin.prototype.isActived = function($li) {
+    return $li.is('.' + this.settings.activeClass);
   };
 
   // Get link label
-  Plugin.prototype.getItemLabel = function($li, level){
+  Plugin.prototype.getItemLabel = function($li, level) {
     return $li.find(this.settings.linkSelector).text();
   };
 
@@ -159,8 +161,8 @@
   };
 
   // Collection method.
-  $.fn.navToSelect = function (options) {
-    return this.each(function () {
+  $.fn.navToSelect = function(options) {
+    return this.each(function() {
       if (!$.data(this, namespace)) {
         $.data(this, namespace, new Plugin(this, options));
       }

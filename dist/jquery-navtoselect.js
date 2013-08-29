1f8001f83a34962ad084918b7a4980136540fa16
iejerry <546842522@qq.com>
2013-08-29 15:24:18 +0800

    update

--- dist/jquery-navtoselect.js
+++ dist/jquery-navtoselect.js
@@ -1,27 +1,137 @@
-/*! jQuery NavToSelect - v0.2.1 - 2013-08-19
+/*! jQuery NavToSelect - v0.2.1 - 2013-08-29
 * https://github.com/amazingSurge/jquery-navToSelect
 * Copyright (c) 2013 amazingSurge; Licensed GPL */
-(function(document, $, undefined) {
-  "use strict";
-
-  var namespace = 'navToSelect';
+(function(window, document, $, undefined) {
+  'use strict';
 
   // Constructor
-  var Plugin = $.navToSelect = function(nav, settings) {
-    this.nav = nav;
-    this.$nav = $(nav);
-
+  var NavToSelect = function(element, options) {
+    this.element = element;
+    this.$element = $(element);
     this._isBuilded = false;
 
-    // Merge the settings given by the user with the defaults
-    this.settings = $.extend({}, Plugin.defaults, settings);
+    this.options = $.extend(NavToSelect.defaults, options);
 
-    // Initialization code to get the ball rolling
+    var self = this;
+    $.extend(self, {
+      init: function() {
+        var items = self.getItems();
+        self.build(items);
+
+        self.$select.on('change', self.options.changeEvent);
+
+      },
+      build: function(items) {
+        self.$select = $('<select />', {
+          'class': self.options.className
+        }).html(self.generateOptionsString(items, 1));
+
+        if (self.options.prependTo === null) {
+          self.$element.after(self.$select);
+        } else {
+          self.$select.prependTo(self.options.prependTo);
+        }
+        self._isBuilded = true;
+      },
+      generateOptionString: function(item, level) {
+        var indent = new Array(level).join(self.options.indentString);
+        if (level !== 1 && self.options.indentSpace) {
+          indent += '&nbsp;';
+        }
+        return '<option value="' + item.value + '"' +
+          (item.linkable === false ? ' data-linkable="false"' : '') +
+          (item.actived === true ? ' selected="selected"' : '') +
+          '>' + indent + item.label + '</option>';
+      },
+      generateOptionsString: function(items, level) {
+        if (level > self.options.maxLevel) {
+          return '';
+        }
+        var options = '';
+        $.each(items, function(index, item) {
+          if (item.linkable === false && typeof item.items !== 'undefined' && level === 1 && self.options.useOptgroup) {
+            options += '<optgroup label="' + item.label + '">';
+            options += self.generateOptionsString(item.items, level + 1);
+            options += '</optgroup>';
+          }
+          if (typeof item.items !== 'undefined') {
+            options += self.generateOptionString(item, level);
+            options += self.generateOptionsString(item.items, level + 1);
+          } else {
+            options += self.generateOptionString(item, level);
+          }
+        });
+        return options;
+      },
+      getItems: function() {
+        var items = [];
+        if (self.options.defaultText) {
+          items = items.concat({
+            value: "#",
+            label: self.options.defaultText,
+            linkable: false
+          });
+        }
+
+        //recursion
+
+        function _getItemsFromList($list, level) {
+          var _items = [];
+
+          $list.children('li').each(function() {
+            var $li = $(this);
+            var item = {
+              value: self.getItemValue($li),
+              label: self.options.getItemLabel.call(self, $li),
+              linkable: self.isLinkable($li),
+              actived: self.isActived($li)
+            };
+            if ($li.children('ul, ol').length) {
+              item.items = [];
+              $li.children('ul, ol').each(function() {
+                item.items = item.items.concat(_getItemsFromList($(this), level + 1));
+              });
+            }
+
+            _items.push(item);
+          });
+          return _items;
+        }
+
+        items = items.concat(_getItemsFromList(self.$element, 1));
+        return items;
+      },
+
+      // Get link url
+      getItemValue: function($li) {
+        return $li.find(self.options.linkSelector).attr('href');
+      },
+
+      // Check if a item can link
+      isLinkable: function($li) {
+        return self.getItemValue($li) !== '#';
+      },
+
+      // Check if a item is actived
+      isActived: function($li) {
+        return $li.is('.' + self.options.activeClass);
+      },
+
+      // Get link label
+      getItemLabel: function($li, level) {
+        return $li.find(self.options.linkSelector).text();
+      },
+
+      // Check if select is builded already
+      isBuilded: function() {
+        return self._isBuilded;
+      }
+    });
     this.init();
   };
 
   // Default options for the plugin as a simple object
-  Plugin.defaults = {
+  NavToSelect.defaults = {
     maxLevel: 4,
     prependTo: null,
     activeClass: 'active',
@@ -31,147 +141,51 @@
     indentSpace: true,
     defaultText: 'Navigate to...',
     useOptgroup: false,
+    namespace: 'navToSelect',
     getItemLabel: function($li) {
-      return $li.find(this.settings.linkSelector).text();
+      return $li.find(this.options.linkSelector).text();
     },
     changeEvent: function() {
       if ($(this).data('linkable') !== false) {
-        Plugin.goTo(this.value);
+        NavToSelect.goTo(this.value);
       }
     }
   };
 
-  // Plugin initializer - prepare your plugin.
-  Plugin.prototype.init = function() {
-    var items = this.getItems();
-    this.build(items);
+  NavToSelect.prototype = {
+    constructor: NavToSelect,
 
-    this.$select.on('change', this.settings.changeEvent);
-
-  };
-
-  Plugin.prototype.build = function(items) {
-    this.$select = $('<select />', {
-      'class': this.settings.className
-    }).html(this.generateOptionsString(items, 1));
-
-    if (this.settings.prependTo === null) {
-      this.$nav.after(this.$select);
-    } else {
-      this.$select.prependTo(this.settings.prependTo);
+    destroy: function() {
+      this.$element.data('NavToSelect', null);
     }
-    this._isBuilded = true;
-  };
-
-  Plugin.prototype.generateOptionString = function(item, level) {
-    var indent = new Array(level).join(this.settings.indentString);
-    if (level !== 1 && this.settings.indentSpace) {
-      indent += '&nbsp;';
-    }
-    return '<option value="' + item.value + '"' +
-      (item.linkable === false ? ' data-linkable="false"' : '') +
-      (item.actived === true ? ' selected="selected"' : '') +
-      '>' + indent + item.label + '</option>';
-  };
-
-  Plugin.prototype.generateOptionsString = function(items, level) {
-    if (level > this.settings.maxLevel) {
-      return '';
-    }
-    var options = '';
-    var self = this;
-    $.each(items, function(index, item) {
-      if (item.linkable === false && typeof item.items !== 'undefined' && level === 1 && self.settings.useOptgroup) {
-        options += '<optgroup label="' + item.label + '">';
-        options += self.generateOptionsString(item.items, level + 1);
-        options += '</optgroup>';
-      }
-      if (typeof item.items !== 'undefined') {
-        options += self.generateOptionString(item, level);
-        options += self.generateOptionsString(item.items, level + 1);
-      } else {
-        options += self.generateOptionString(item, level);
-      }
-    });
-    return options;
-  };
-
-  Plugin.prototype.getItems = function() {
-    var items = [];
-    if (this.settings.defaultText) {
-      items = items.concat({
-        value: "#",
-        label: this.settings.defaultText,
-        linkable: false
-      });
-    }
-    var self = this;
-
-    //recursion
-
-    function _getItemsFromList($list, level) {
-      var _items = [];
-
-      $list.children('li').each(function() {
-        var $li = $(this);
-        var item = {
-          value: self.getItemValue($li),
-          label: self.settings.getItemLabel.call(self, $li),
-          linkable: self.isLinkable($li),
-          actived: self.isActived($li)
-        };
-        if ($li.children('ul, ol').length) {
-          item.items = [];
-          $li.children('ul, ol').each(function() {
-            item.items = item.items.concat(_getItemsFromList($(this), level + 1));
-          });
-        }
-
-        _items.push(item);
-      });
-      return _items;
-    }
-
-    items = items.concat(_getItemsFromList(this.$nav, 1));
-    return items;
-  };
-
-  // Get link url
-  Plugin.prototype.getItemValue = function($li) {
-    return $li.find(this.settings.linkSelector).attr('href');
-  };
-
-  // Check if a item can link
-  Plugin.prototype.isLinkable = function($li) {
-    return this.getItemValue($li) !== '#';
-  };
-
-  // Check if a item is actived
-  Plugin.prototype.isActived = function($li) {
-    return $li.is('.' + this.settings.activeClass);
-  };
-
-  // Get link label
-  Plugin.prototype.getItemLabel = function($li, level) {
-    return $li.find(this.settings.linkSelector).text();
-  };
-
-  // Check if select is builded already
-  Plugin.prototype.isBuilded = function() {
-    return this._isBuilded;
   };
 
   // Go to page
-  Plugin.goTo = function(url) {
+  NavToSelect.goTo = function(url) {
     document.location.href = url;
   };
 
   // Collection method.
   $.fn.navToSelect = function(options) {
-    return this.each(function() {
-      if (!$.data(this, namespace)) {
-        $.data(this, namespace, new Plugin(this, options));
-      }
-    });
+    if (typeof options === 'string') {
+      var method = options;
+      var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : undefined;
+
+      return this.each(function() {
+        var api = $.data(this, 'navToSelect');
+
+        if (api && typeof api[method] === 'function') {
+          api[method].apply(api, method_arguments);
+        }
+      });
+    } else {
+      return this.each(function() {
+        var api = $.data(this, 'navToSelect');
+        if (!api) {
+          api = new NavToSelect(this, options);
+          $.data(this, 'navToSelect', api);
+        }
+      });
+    }
   };
-}(document, jQuery));
+}(window, document, jQuery));
